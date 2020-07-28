840b4adb4bf4826da95954ae6d24d728c325e5b0
Phoenix Zhong <phoenixson191@gmail.com>
2020-07-28 10:51:24 +0800

    add comments for 3kyuLastdigitofahugenumber.rb

--- 3kyuLastdigitofahugenumber.rb
+++ 3kyuLastdigitofahugenumber.rb
@@ -1,29 +1,32 @@
+# Requirement
+# For a given list [x1, x2, x3, ..., xn] compute the last (decimal) digit of x1 ^ (x2 ^ (x3 ^ (... ^ xn))).
+# e.g.
+# last_digit({3, 4, 2}, 3) == 1
+
+# Thinking
+# 最后一位的数字这类题有一个规律，即每4次开始重复
+# 比如，7: 7,9,3,1,7...
+
 def last_digit(lst)
-  return 1 if lst.empty?
-  return last_digit_core(lst[0] % 10, lst[1]) if lst.size == 2
-  
-  base = lst.shift % 10
-  return 1 if base == 1
-  return base % 10 if lst.empty?
+  n = 1
 
-  a, b = lst.pop(2)
-  
-  a = a % 100 + 100 if a > 100
-  b = b % 100 + 100 if b > 100
+  # the ones digit is at most a 4 cycle for any a^b
+  # therefor (a ^ b) % 10 == (a ^ (b % 4)) % 10
+  # special case when b is non-zero and divisible by 4, we should use 4 instead
 
-  k = a ** b
-  k = k % 100 + 100 if k > 100
+  # x1 ^ (x2 ^ (x3 ^ x4)) % 10
+  # let a = x1
+  #     b = x2 ^ (x3 ^ x4)
+  #
+  # a ^ b % 10 == a ^ (b % 4) % 10
+  #
+  # b % 4 = x2 ^ (x3 ^ x4) % 4 = (x2 ^ (x3 ^ x4) % 4) % 10
+  # continue ...
 
-  while lst.size.positive?
-    m = lst.pop
-    k = m ** k
-    k = k % 100 + 100 if k > 100
+  lst.reverse.each do |x|
+    power = n < 4 ? n : n % 4 + 4
+    n = x ** power
   end
-  
-  last_digit_core(base, k)
-end
 
-
-def last_digit_core(n1, n2)
-  n2.zero? ? 1 : ((n1 % 10) ** (n2 % 4 + 4)) % 10
+  n % 10
 end
